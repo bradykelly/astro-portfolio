c6cb0c4fe959a37f292e8e0778e1674030d5e0c7
Brady Kelly <brady@bradykelly.net>
2024-03-02 19:30:22 +0200

    Pulling in CV parts from astro-vitae template

---
+++ src/content/cv/json-resume.ts
@@ -0,0 +1,21 @@
+export interface Work {
+	startDate: string
+	endDate: string
+	position: string
+	mame: string
+	url: string
+	location: string
+	summary: string
+	highlights: string[]
+}
+
+export interface Education {
+	institution: string;
+	area: string;
+	studyType: string;
+	startDate: string;
+	endDate: string;
+	score: string;
+	courses: string[];
+}
+
