b7089b2fe052b77fe77303f04287fc506ce7e642
Brady Kelly <brady@bradykelly.net>
2024-03-03 02:13:30 +0200

    JR interfaces defined

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+import { ZodString, z } from 'astro/zod';
+
+export class Iso8601 extends ZodString {
+    // TODO Validation voodoo: ISO8601-1:2019
+    // Schema: Pattern '^([1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]|[1-2][0-9]{3}-[0-1][0-9]|[1-2][0-9]{3})$'
+}
+
+export interface Location {
+    address: string,
+    postalCode: string,
+    city: string,
+    // TODO Validation voodoo: ISO-3166-1 ALPHA-2, e.g. US, AU, IN
+    countryCode: string, 
+    region: string,
+    [k: string]: unknown
+}
